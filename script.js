ee6a4109a382b921317e4bd0f2e0c0b236c7dba4
ian <odhiamboian096>
2025-11-04 11:52:27 +0300

    anoder banger

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-// //single element
-// //1.getElementByd('my-form)
-// // console.log(document.getElementById('my-form'));
+// const button = document.querySelector(".btn");
+// button.addEventListener("mouseover", onClick);
+// function onClick(event) {
+//   event.preventDefault();
+//   document.querySelector("#my-form").style.background = "#14b2f0ff";
+//   document.querySelector("body").classList.add("bg-dark");
+//   document.querySelector(".items").children[2].innerHTML = "<h2>Hello</h2>";
+// }
 
-// // const form = document.getElementById("my-form");
-// // console.log(form);
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
 
-// //2.document.querySelector()
+// myForm.addEventListener("submit", onSubmit);
+// function onSubmit(event) {
+//   event.preventDefault();
+//   if (nameInput.value === "" || emailInput.value === "") {
+//     msg.classList.add("error");
+//     msg.textContent = "Please input both fields";
+//     setTimeout(() => {
+//       msg.classList.remove("error");
+//       msg.textContent = "";
+//     }, 3000);
+//     return;
+//   }
+// }
 
-// const header = document.querySelector("h1"); //this will pretty much grab anything on our html (tag,classes,Id)
+myForm.addEventListener("submit", onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.textContent = "Please enter both fields";
+    msg.classList.add("error");
+    setTimeout(() => {
+      msg.textContent = "";
+      msg.classList.remove("error");
+    }, 3000);
+  } else {
+    const list = document.createElement("li");
+    list.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+    list.appendChild(li);
 
-// console.log(header);
 
-// console.log(document.querySelector(".container"));
-
-// console.log(document.getElementsByClassName("item"));
-
-// console.log(document.getElementsByTagName("li"));
-
-// console.log(document.querySelectorAll(".item")); //this is a selector for grabbing multiple elements and provides a node list similar to an array where high array methods and properties can also be applied
-
-// const items = document.querySelectorAll(".item");
-// items.forEach(function (item) {
-//   console.log(item);
-// });
-
-// const ul = document.querySelector(".items");
-// console.log(ul);
-// // ul.lastElementChild.remove();
-// ul.firstElementChild.textContent = "Hello";
-// ul.children[1].innerText = "Ian";
-// ul.lastElementChild.innerHTML = "<h1>Hello World</h1>";
-// console.log(ul);
-
-// // ul.lastElementChild.remove()
-
-// ul.children[0].innerText = "Clinton";
-// // ul.children[2].innerText = "Clinton";
-
-// const btn = document.querySelector(".btn");
-// btn.style.background = "red";
-
-const btn = document.querySelector(".btn");
-btn.addEventListener("mouseout", function (e) {
-  e.preventDefault();
-  document.querySelector("#my-form").style.background = "#3681b3ff";
-  document.querySelector("body").classList.add("bg-dark");
-  document.querySelector(".items").lastElementChild.innerHTML =
-    "<h2>Hello</h2>";
-});
+    //clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
